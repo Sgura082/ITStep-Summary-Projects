e9494b41c53472b3787f907a9dd8404f3637e864
Saba gurashvili <Sgura082@gmail.com>
2024-03-06 13:56:52 +0400

    added code to create linkedlist type structure using classes from 'Storage_structure'

--- Warehouse_Grid.py
+++ Warehouse_Grid.py
@@ -1,9 +1,10 @@
 import tkinter as tk
-import math
+import Storage_structure as Storage
 
 #--------------------------CLASSES-----------------------------------
 class WarehouseApp:
     def __init__(self, master):
+        self.warehouse_struct = Storage.Warehouse("W01")
         self.master = master
         self.master.title("Bookcase")
         # Create a frame for the Warehouse
@@ -26,21 +27,26 @@ class WarehouseApp:
                 CN = 5
             self.Shelf_frame = tk.Frame(self.Warehouse_frame, bg="lightgray", bd=2, relief=tk.SUNKEN)
             self.Shelf_frame.grid(row=RN, column=CN, padx=10, pady=3, columnspan =4)
+
+            Shelf_title = tk.Label(self.Shelf_frame, text=f"A0{stellN + 1}", bg="skyblue", padx=10, pady=5,
+                                   font=("Arial 14 bold"), width=20, height=1, relief=tk.RAISED)
+            Shelf_title.grid(row=RN, column=CN, padx=5, pady=3, columnspan=4)
+            shelf_new = Storage.W_Shelf(self.warehouse_struct,f"A0{stellN + 1}")
+            self.warehouse_struct.Shelfs_in_warehouse.append(shelf_new)
             for j in range(3):
 
-                Shelf_title = tk.Label(self.Shelf_frame , text=f"A0{stellN+1}", bg="skyblue", padx=10, pady=5,
-                                          font=("Arial 14 bold"),width=38, height=1,relief=tk.RAISED)
-                Shelf_title.grid(row=RN, column=CN, padx=5, pady=3, columnspan =4)
 
-                Level_title = tk.Label(self.Shelf_frame , text=f"Level {abs(j-3)} | #{Shelf_title.cget("text")}.{abs(j-3)}", bg="lightblue", padx=10, pady=5)
+                Level_title = tk.Label(self.Shelf_frame , text=f"Level {abs(j-3)}", bg="lightblue", padx=10, pady=5)
                 Level_title.grid(row=RN+j+1, column=CN, padx=5, pady=3)
 
 
-                for i in range(3):
-                    shelf1 = tk.Button(self.Shelf_frame , text=f"{Shelf_title.cget("text")}.{abs(j-3)}.{abs(i+1)}:    FREE", bg="lightgreen", padx=10, pady=5,
+
+                shelf_cell = tk.Button(self.Shelf_frame , text=f"{Shelf_title.cget("text")}.{abs(j-3)}:    FREE",width=15,anchor="w", bg="lightgreen", padx=10, pady=5,
                                      relief=tk.RAISED)
-                    shelf1.grid(row=RN+j+1, column=CN+i+1, padx=5, pady=3)
-                    self.shelves.append(shelf1)
+                shelf_cell.grid(row=RN+j+1, column=CN+1, padx=5, pady=3)
+                cell_new = Storage.W_Cell(shelf_cell.cget("text"))
+                self.shelves.append(shelf_cell)
+                shelf_new.add_cell(cell_new)
             columnN += 4
             rowN += 4
             stellN += 1
@@ -54,8 +60,6 @@ class WarehouseApp:
     def insert_containers(self):
         shelf_index = 1
         cell_name =self.shelves[shelf_index].cget("text")[:9]
-
-        print(cell_name)
         self.shelves[shelf_index].config(text=f"{cell_name}OCCUPIED",bg="RED",relief=tk.SUNKEN)
 
 
@@ -64,10 +68,12 @@ def main():
     root = tk.Tk()
     root.title("My Database")
     app = WarehouseApp(root)
+    a= app.warehouse_struct.Shelfs_in_warehouse
+    for i in a:
+        print(i)
     app.insert_containers()
     root.mainloop()
 # def load_container(cont):
-
 
 
 if __name__ == "__main__":

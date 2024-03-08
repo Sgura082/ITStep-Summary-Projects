2f73ea0ed57f77e65dcb1f695f7cb8b2ce9e77d6
Saba gurashvili <Sgura082@gmail.com>
2024-03-08 23:48:05 +0400

    added calculator task, started the library management

--- SubProjects/SubProjects.py
+++ SubProjects/SubProjects.py
@@ -2,7 +2,7 @@ import re
 
 # --------------------TASK DESCRIPTIONS-------------------------------
 # ------Task1: Calculator
-# ------Task2:
+# ------Task2: Library management
 # ------Task3:
 # ------Task4:
 
@@ -40,35 +40,122 @@ class Task():
 # -----------CUSTOM FUNCTIONS----------------
 
 # -----------TASK OBJECTS----------------
-Task01 = Task("x")
+Task01 = Task("Calculator")
 def task1_body():
     pass
     # ----------------Task Variables----------------------------------
-    operations = {"+": lambda x,y: x+y,
-                  }
+    Calculator = {'+': {"Name": "   Addition", "Formula": lambda x, y: x + y},
+                  '-': {"Name": "   Subtraction", "Formula": lambda x, y: x - y},
+                  '/': {"Name": "   Division", "Formula": lambda x, y: x / y},
+                  '*': {"Name": "   Multiplication", "Formula": lambda x, y: x * y},
+                  '//': {"Name": "  Floor division", "Formula": lambda x, y: x // y},
+                  '%': {"Name": "   Modulus", "Formula": lambda x, y: x % y},
+                  '**': {"Name": "  Exponentiation", "Formula": lambda x, y: x ** y},
+                  'root': {"Name": "Getting root", "Formula": lambda x, y: x ** (1 / y)}}
     # ----------------Task Classes------------------------------------
     # ----------------Task Functions----------------------------------
+    def input_loop_with_validation(N,opera =""):
+        Control_for_zero_division = ["/", "//", "%"]
+        while True:
+            userNum = input(f"Enter number {N}: ")
+            if (opera in Control_for_zero_division) and float(userNum) == 0:
+                print("You can't divide by 0. Enter other number!!!")
+                continue
+            elif re.match(r'^([\.\d]+)$', userNum):
+                break
+            else:
+                print("ERROR Wrong input. Please enter only INT number.")
+        return userNum
+
     # ----------------Task BODY---------------------------------------
+    print("\n-----------------------------------")
+    OperationList = list(Calculator.keys())
+    prompt = [" " + i + " " + Calculator.get(i).get("Name") for i in OperationList]
+    print("\n------PROMPT LIST------", *prompt, sep="\n")
+    while True:
+        userOper = input("\nEnter an operation you want to conduct (use prompt above): ")
+        if userOper in OperationList:
+            break
+        else:
+            print("No such operation found!!! Check prompt list at the start of program!!")
+    userNum1 = float(input_loop_with_validation(1))
+    userNum2 = float(input_loop_with_validation(2, userOper))
+    result = Calculator.get(userOper).get("Formula")(userNum1, userNum2)
+    print(f"\nResult of calculation: \n{userNum1} {userOper} {userNum2} = {result}")
 Task01.write_function(task1_body)
 
-Task02 = Task("x")
+#-------------------------------------------------------------------------------------------
+
+Task02 = Task("Library management")
 def task2_body():
     pass
     # ----------------Task Variables----------------------------------
     # ----------------Task Classes------------------------------------
+    class Book():
+        def __init__(self,title,author,issue_date,genre,language,rating):
+            self.title =title
+            self.author = author
+            self.issue_date = issue_date
+            self.genre =genre
+            self.language = language
+            self.critics_rating = rating #Rating of a book from min 1 to max 5
+            self.borrower = None #Name of person who borrowed the book from the library
+            self.status = "IN Library"
+            self.time_out_of_library = 0.0  # Time the book spent outside library while being borrowed by the reader
+    class Reader():
+        def __init__(self,name,id,student_status):
+            self.name = name
+            self.id = id
+            self.student_status=student_status
+    class Library():
+        def __init__(self,name):
+            self.name = name
+            self.book_list = []
+            self.book_borrowers = {'white_list':[{'Borrower': object,
+                                                  'Number of books borrowed': 0,
+                                                  'Number of overdue books': 0}],
+                                   'blacklist': [{'Borrower': object,
+                                                  'Number of books borrowed': 0,
+                                                  'Number of overdue books': 0}],
+                                   }
+
+        def add_book(self,book):
+            assert isinstance(book,Book)
+            self.book_list.append(book)
+        def search_and_disp_books(self,title="0",author="0",issue_date="0",genre="0",laguage="0",rating="0"):
+            display_list =[]
+            if title == "0":
+                title_search = 0
+            if author == "0":
+                author_search = 0
+            if issue_date == "0":
+                issue_date_search = 0
+            if author == "0":
+                author_search = 0
+            auth_check = 1
+
+            for book in self.book_list:
+                if title in book.title:
+                    display_list.append(book)
+
+
     # ----------------Task Functions----------------------------------
     # ----------------Task BODY---------------------------------------
+
+
 Task02.write_function(task2_body)
 
+
+#-------------------------------------------------------------------------------------------
 Task03 = Task("x")
-
-
 def task3_body():
     pass
     # ----------------Task Variables----------------------------------
     # ----------------Task Classes------------------------------------
     # ----------------Task Functions----------------------------------
     # ----------------Task BODY---------------------------------------
+
+
 Task03.write_function(task3_body)
 
 # -------------MAIN CODE---------------------------------------------------------------------

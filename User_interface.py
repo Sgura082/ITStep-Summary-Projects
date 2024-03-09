d24e14a14f0d1b7cae0b8f7a496786357d39544c
Saba gurashvili <Sgura082@gmail.com>
2024-03-09 18:55:25 +0400

    Added descriptions and comments

--- User_interface.py
+++ User_interface.py
@@ -2,24 +2,22 @@ import tkinter as tk
 import Warehouse_Grid as WG
 import Inbound_operations_window as inbound
 import Outbound_operations_window as outbound
-import main as BOSS
+import Operations as oper
 #-------------Global Variables--------------
 #-------------Main Window----------------
 
 def SaveToFile():
-    BOSS.save_to_file()
+    oper.save_to_file()
 
 def OpenFile():
-    BOSS.Open_file()
-def Inbound():
+    oper.Open_file()
+def Inbound(): #Summons window from Inbound_operations_window
     inbound.main()
 
-def Outbound():
+def Outbound(): #Summons window from Outbound_operations_window
     outbound.main()
 
-
-
-# ------------Creates the WELCOME window
+# ------------Creates the Main Menu window------------------------------
 Grid = WG.main()
 Main_Menu = tk.Tk()
 Main_Menu.title("WAREHOUSE APP 1.0")
@@ -27,18 +25,25 @@ Main_Menu.title("WAREHOUSE APP 1.0")
 
 name_label = tk.Label(Main_Menu, text=f"You are working in warehouse: {WG.Warehouse[0].name}",font=("Arial 15 bold"))
 name_label.pack()
-in_button = tk.Button(Main_Menu , text=f"IN Operations",font=("Arial 10 bold"),width=15,anchor="c", bg="gray", padx=10, pady=5,
+
+# ------------Opens Inbound operations window where user can store boxes in warehouse----------------------
+in_button = tk.Button(Main_Menu , text=f"IN Operations",font=("Arial 10 bold"),width=15,anchor="c", bg="Green", padx=10, pady=5,
                                      relief=tk.RAISED,command =Inbound)
-out_button = tk.Button(Main_Menu , text=f"OUT Operations",font=("Arial 10 bold"),width=15,anchor="c", bg="gray", padx=10, pady=5,
+in_button.pack()
+
+# ------------Opens Outbound operations window where user can take out boxes from warehouse----------------
+out_button = tk.Button(Main_Menu , text=f"OUT Operations",font=("Arial 10 bold"),width=15,anchor="c", bg="tomato", padx=10, pady=5,
                                      relief=tk.RAISED, command =Outbound)
+out_button.pack()
+
+# ------------Open Json file to retrieve warehouse data from previous session------------------------------
 Open_file_button = tk.Button(Main_Menu , text=f"Open file",font=("Arial 10 bold"),width=15,anchor="c", bg="gray", padx=10, pady=5,
                                      relief=tk.RAISED,command =OpenFile)
+Open_file_button.pack()
+
+# ------------Save current session data to Json file ------------------------------------------------------
 SaveTo_file_button = tk.Button(Main_Menu , text=f"Save to file",font=("Arial 10 bold"),width=15,anchor="c", bg="gray", padx=10, pady=5,
                                      relief=tk.RAISED, command =SaveToFile)
-
-in_button.pack()
-out_button.pack()
-Open_file_button.pack()
 SaveTo_file_button.pack()
 
 

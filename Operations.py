971358d3638a00a59a31d6048a1529e963fa2382
Saba gurashvili <Sgura082@gmail.com>
2024-03-09 18:15:51 +0400

    Added comments to code

--- Operations.py
+++ Operations.py
@@ -1,12 +1,8 @@
 
 import copy
-
-import pandas as pd
-
 import Warehouse_Grid as WG
 import json
 import re
-
 global message_to_be_displayed
 message_to_be_displayed = None
 
@@ -21,20 +17,23 @@ def store_box_in_warehouse(box, content):
     :return:
     """
     warehouse = WG.Warehouse[0]  # gets the main class object (Warehouse, which stores all data about shelves, cells and
-    # their contents
-    index = 0
+                                    # their contents
+
     global message_to_be_displayed
-    if len(box) > 6:
+    #-----Box number validation------------------------------------------------------------------------------
+
+    if len(box) > 6: #-----Box number- length validation------------------------------------------------------
         message_to_be_displayed = "Box number cannot have more than 6 digits!!!!\nPlease enter proper Number"
         return
-    if re.fullmatch(r"\d+", box) == None:
+    if re.fullmatch(r"\d+", box) == None: #-----Box number- only digits validation---------------------
         message_to_be_displayed = "Box number must contain ONLY digits!!!!\nPlease enter proper Number"
         return
+    # -----Box number conversion to B000001 standard
     if len(box) < 6:
         box = "0" * (6 - len(box)) + box
     box = "B" + box
     for shelf in warehouse.Shelfs_in_warehouse:  # search for free cell in warehouse
-        #1-----------------------Checking if box with such number already is stored-----------------
+        #/1-----------------------Checking if box with such number already is stored-----------------
         current_cell = shelf.first_cell
         while True:
             if current_cell.cell_box != None:
@@ -44,7 +43,7 @@ def store_box_in_warehouse(box, content):
                     return
             if current_cell.cell_above == None:
                 break
-            current_cell = current_cell.cell_above
+            current_cell = current_cell.cell_above #1/-----------------------------------------------
         #2------------------------Checking for empty cell--------------------------------------------
         current_cell = shelf.first_cell
         while True:
@@ -55,7 +54,7 @@ def store_box_in_warehouse(box, content):
                 return
             if current_cell.cell_above == None:
                 break
-            current_cell = current_cell.cell_above
+            current_cell = current_cell.cell_above #2/-----------------------------------------------
     message_to_be_displayed = "No free cells were found!!!"
 
 
@@ -68,16 +67,19 @@ def take_box_frome_warehouse(box):
     """
     warehouse = WG.Warehouse[0]
     global message_to_be_displayed
-    if len(box) > 6:
+    # -----Box number validation-----------------------------------------------------------------------------
+
+    if len(box) > 6:#-----Box number- length validation------------------------------------------------------
         message_to_be_displayed = "Box number cannot have more than 6 digits!!!!\nPlease enter proper Number"
         return
-    if re.fullmatch(r"\d+", box) == None:
+    if re.fullmatch(r"\d+", box) == None:#-----Box number- only digits validation---------------------
         message_to_be_displayed = "Box number must contain ONLY digits!!!!\nPlease enter proper Number"
         return
+    # -----Box number conversion to B000001 standard---------------------------------------------------------
     if len(box) < 6:
         box = "0" * (6 - len(box)) + box
     box = "B" + box
-    # search for cell with the box in warehouse
+    # search for cell with the box in warehouse---------------------------------------------------------------
     for shelf in warehouse.Shelfs_in_warehouse:
         for cell in shelf.shelf_cells:
             if cell.cell_box:
@@ -92,7 +94,7 @@ def take_box_frome_warehouse(box):
 # --------------------------------------------------------------------------------------------------
 def encoder(warehouse):
     """
-    Used to encode cell data for saving to a CSV File
+    Used to encode data of cells in warehouse so that they can be stored as dict in json file.
     :param warehouse: Warehouse class object containing information about all cells.
     :return:
     """
@@ -113,16 +115,14 @@ def encoder(warehouse):
             data.append(cell_datac)
     return data
 
-
+# --------------------------------------------------------------------------------------------------
 def decoder(data):
     """
-    Reads data from saved CSV file and changes existing W_Cell class objects data based on values in file.
-    :param data: data (dictionary) from CSV file.
+    Decodes Json data and updates W_cell class objects based on the data in file.
+    :param data: data (dictionary) from Json file.
     :return:
     """
-
     warehouse1 = WG.Warehouse[0]
-    print(f"1{warehouse1}")
     for shelf in warehouse1.Shelfs_in_warehouse:
         for cell in shelf.shelf_cells:
             for cell_from_file in data:
@@ -132,17 +132,22 @@ def decoder(data):
                     if cell_box_name_from_file == "None":
                         continue
                     cell.put_box_in_cell(cell_box_name_from_file,cell_box_content_from_file)
-
-
-
+# --------------------------------------------------------------------------------------------------
 def save_to_file():
+    """
+    Used by button in User_interface to save current session status of cells in json file
+    :return:
+    """
     data = encoder(WG.Warehouse[0])
-    with open ('data.json', "w") as csv_file:
-        json.dump(data, csv_file)
-
-
-
+    with open ('data.json', "w") as Json_file:
+        json.dump(data, Json_file)
+# --------------------------------------------------------------------------------------------------
 def Open_file():
-    with open ('data.json', "r") as csv_file1:
-        data = json.load(csv_file1)
+    """
+    Used by button in User_interface to open Json file and update W_cell class object data
+    based on previous session data stored in file
+    :return:
+    """
+    with open ('data.json', "r") as Json_file1:
+        data = json.load(Json_file1)
         decoder(data)

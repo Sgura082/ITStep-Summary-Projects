2244298a1482e1b04fc78feb6726deaea2a49f0c
Saba gurashvili <Sgura082@gmail.com>
2024-03-09 17:29:23 +0400

    Added encoder & decoder and Write to & Read from file functions. so that user can store data about storage in warehouse

--- Operations.py
+++ Operations.py
@@ -1,89 +1,133 @@
-import Warehouse_Grid as WG
+
+import copy
+
 import pandas as pd
+
+import Warehouse_Grid as WG
+import json
 import re
 
 global message_to_be_displayed
 message_to_be_displayed = None
-def store_box_in_warehouse(box,content):
+
+
+# --------------------------------------------------------------------------------------------------
+def store_box_in_warehouse(box, content):
     """
     Searches for a free cell inside the warehouse among its shelves. when such is found cell class method
     put_box_in_cell is called to store the box and its contents within the cell.
-    :param box: Number of the stored box can be either string or Box class object.
+    :param box: Number of the box which is to be stored (string).
     :param content: Description of the content of box. String type.
     :return:
     """
-    warehouse = WG.Warehouse[0] # gets the main class object (Warehouse, which stores all data about shelves, cells and
-                                # their contents
+    warehouse = WG.Warehouse[0]  # gets the main class object (Warehouse, which stores all data about shelves, cells and
+    # their contents
     index = 0
     global message_to_be_displayed
     if len(box) > 6:
         message_to_be_displayed = "Box number cannot have more than 6 digits!!!!\nPlease enter proper Number"
         return
-    if re.fullmatch(r"\d+",box)== None:
+    if re.fullmatch(r"\d+", box) == None:
         message_to_be_displayed = "Box number must contain ONLY digits!!!!\nPlease enter proper Number"
         return
     if len(box) < 6:
-        box = "B"+"0"*(6-len(box))+box
-    for shelf in warehouse.Shelfs_in_warehouse: #search for free cell in warehouse
+        box = "0" * (6 - len(box)) + box
+    box = "B" + box
+    for shelf in warehouse.Shelfs_in_warehouse:  # search for free cell in warehouse
         for cell in shelf.shelf_cells:
             if cell.cell_box == None:
                 txt = cell.label.cget("text")
-                cell.label.configure(text=f"{txt[:5]}: {box}", bg="red")
-                cell.put_box_in_cell(box,content)
+                cell.put_box_in_cell(box, content)
                 message_to_be_displayed = f"Your box N: {box} was stored in cell N: {txt[:5]}"
                 return
             index += 1
     message_to_be_displayed = "No free cells were found!!!"
+
+
+# --------------------------------------------------------------------------------------------------
 def take_box_frome_warehouse(box):
+    """
+    Searches for a cell containing the Box class object with the same name  Upon finding one clears the cell contents.
+    :param box: The number of a box that needs to be taken out from warehouse.
+    :return:
+    """
     warehouse = WG.Warehouse[0]
     global message_to_be_displayed
     if len(box) > 6:
         message_to_be_displayed = "Box number cannot have more than 6 digits!!!!\nPlease enter proper Number"
         return
-    if re.fullmatch(r"\d+",box)== None:
+    if re.fullmatch(r"\d+", box) == None:
         message_to_be_displayed = "Box number must contain ONLY digits!!!!\nPlease enter proper Number"
         return
     if len(box) < 6:
-        box = "B"+"0"*(6-len(box))+box
-    #search for cell with the box in warehouse
-    index = 0
+        box = "0" * (6 - len(box)) + box
+    box = "B" + box
+    # search for cell with the box in warehouse
     for shelf in warehouse.Shelfs_in_warehouse:
         for cell in shelf.shelf_cells:
             if cell.cell_box:
                 if cell.cell_box.name == box:
                     txt = cell.label.cget("text")
-                    cell.label.configure(text=f"{txt[:5]}: FREE", bg="lightgreen")
                     cell.clear_contents()
                     message_to_be_displayed = f"Your box N: {box} was removed from cell N: {txt[:5]}"
                     return
-                index += 1
     message_to_be_displayed = "No such BOX was found!!!"
 
+
+# --------------------------------------------------------------------------------------------------
 def encoder(warehouse):
+    """
+    Used to encode cell data for saving to a CSV File
+    :param warehouse: Warehouse class object containing information about all cells.
+    :return:
+    """
+    data = []
 
-    shelf_dict_lst =[]
     for shelf in warehouse.Shelfs_in_warehouse:
-        Cells_dict_lst = []
         for cell in shelf.shelf_cells:
-            Cells_dict ={"Cell_name": cell.name,
-                         "Cell_parent": cell.parent,
-                         "Cell_Box": cell.cell_box,
-                         "Cell_Status": cell.status,
-                         "Cell_Label": cell.label}
-            Cells_dict_lst.append(Cells_dict)
-
-        shelf_dict = {"Shelf_Name": shelf.name,
-                      "Cells_in_shelf": Cells_dict_lst}
-        shelf_dict_lst.append(shelf_dict)
-    data = {"Warehouse_name": warehouse.name, "Shelves_in_warehouse": shelf_dict_lst}
+            Box_name = "None"
+            Box_contents = "None"
+            if cell.cell_box != None:
+                Box_name =cell.cell_box.name
+                Box_contents = cell.cell_box.contents
+            cell_data = {"Cell_name": cell.name,
+                         "Cell_Box_Name": Box_name,
+                         "Cell_Box_Contents": Box_contents,
+                         "Cell_Status": cell.status}
+            cell_datac =copy.deepcopy(cell_data)
+            data.append(cell_datac)
     return data
-def save_to_file():
-    df = pd.DataFrame(encoder(WG.Warehouse[0]))
-    df.to_csv('data.csv', index=False)
-def Open_file():
-    df = pd.read_csv('data.csv', index_col=1)
-    a = df.to_dict()
-    print(df)
-    # for shelf in WG.Warehouse[0].Shelfs_in_warehouse:
-    #     for cell in shelf.shelf_cells:
 
+
+def decoder(data):
+    """
+    Reads data from saved CSV file and changes existing W_Cell class objects data based on values in file.
+    :param data: data (dictionary) from CSV file.
+    :return:
+    """
+
+    warehouse1 = WG.Warehouse[0]
+    print(f"1{warehouse1}")
+    for shelf in warehouse1.Shelfs_in_warehouse:
+        for cell in shelf.shelf_cells:
+            for cell_from_file in data:
+                if cell_from_file['Cell_name'] == cell.name:
+                    cell_box_name_from_file = cell_from_file['Cell_Box_Name']
+                    cell_box_content_from_file = cell_from_file['Cell_Box_Contents']
+                    if cell_box_name_from_file == "None":
+                        continue
+                    cell.put_box_in_cell(cell_box_name_from_file,cell_box_content_from_file)
+
+
+
+def save_to_file():
+    data = encoder(WG.Warehouse[0])
+    with open ('data.json', "w") as csv_file:
+        json.dump(data, csv_file)
+
+
+
+def Open_file():
+    with open ('data.json', "r") as csv_file1:
+        data = json.load(csv_file1)
+        decoder(data)
